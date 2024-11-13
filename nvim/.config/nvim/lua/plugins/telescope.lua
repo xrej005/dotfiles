15776ddd94ef14d3b8647b15f85d01537ee7a276
xrej005 <74497492+xrej005@users.noreply.github.com>
2024-11-13 10:48:34 +0100

    NG+

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,12 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+            theme = "ivy",
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
